0ecac5929bae437ca3eb6fea2ada662dc3bfaff6
abialek677 <adamb@DESKTOP-H7R61A7>
2024-12-02 23:25:09 +0100

    added mains

---
+++ album-management/src/main/java/pg/inf/sem5/gr2/albummanagement/AlbumManagementApplication.java
@@ -0,0 +1,13 @@
+package pg.inf.sem5.gr2.albummanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbumManagementApplication.class, args);
+    }
+
+}
